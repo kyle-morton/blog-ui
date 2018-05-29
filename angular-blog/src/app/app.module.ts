8cedc5034514f69461577d6409fce01af3d6bf06
Kyle Morton <kylemortondevelopment@gmail.com>
2018-05-29 16:13:49 -0500

    added browerAnimationsModule for transitioning between states

--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { Http, HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -31,6 +32,7 @@ import { PreviewPostComponent } from './preview-post/preview-post.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
     AppRoutingModule
   ],
